d3b2db637e26e9f8a92dbd0e782bddd86e05300d
Jonatas Alves <jonatasalves@id.uff.br>
2024-08-11 22:09:05 -0300

    feat: node + mysql listando nomes adicionados em people

--- node/index.js
+++ node/index.js
@@ -1,24 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database:'nodedb'
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb',
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
+const mysql = require('mysql2');
+const connection = mysql.createConnection(config);
 
-const sql = `INSERT INTO people(name) values('Wesley')`
-connection.query(sql)
-connection.end()
+const Charlatan = require('charlatan');
+Charlatan.setLocale('pt-BR');
 
+app.get('/', (req, res) => {
+  connection.query(
+    `INSERT INTO people (name) VALUES ('${Charlatan.Name.name()}')`
+  );
 
-app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+  connection.query(`SELECT name FROM people`, (error, results, fields) => {
+    res.send(`
+      <h1>Full Cycle Rocks!</h1>
+      <ul>
+        ${
+          results.length > 0
+            ? results.map((el) => `<li>${el.name}</li>`).join('')
+            : ''
+        }
+      </ul>
+    `);
+  });
+});
 
-app.listen(port, ()=> {
-    console.log('Rodando na porta ' + port)
-})
+app.listen(port, () => {
+  console.log('Rodando na porta ' + port);
+});
